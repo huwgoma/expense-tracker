3c66f24e32a7e6119bfe16c703fdd5f13d589353
Hugo Ma <huwgoma@gmail.com>
2025-01-16 11:14:27 -0500

    Add GET expenses/new route and preliminary form

--- app.rb
+++ app.rb
@@ -3,7 +3,24 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'pry' if development?
 
+configure do
+  #set :erb, :escape_html => :true
+end
+########################
+#  Application Routes  #
+########################
 
+
+# # # # # # # # 
+#  Expenses   #
+# # # # # # # # 
+# Dashboard
 get '/' do
-  'Hello'
+  @expenses = ['Purchase 1', 'Purchase 2']
+  erb :dashboard
+end
+
+# Create new expense
+get '/expenses/new' do
+  erb :new_expense
 end
---
+++ lib/expense.rb
@@ -0,0 +1,4 @@
+class Expense
+  
+  
+end
